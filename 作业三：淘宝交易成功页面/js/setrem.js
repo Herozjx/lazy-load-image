0b66b255ce2e4d2e0cc526b5feeafbeb470d636b
zjx <1193028309@qq.com>
2022-11-11 14:11:36 +0800

    初始化作业懒加载

---
+++ 作业三：淘宝交易成功页面/js/setrem.js
@@ -0,0 +1,18 @@
+function setRem() {
+    // 设计稿宽度和设备宽度
+    var uiW = 640,
+        winW = document.documentElement.clientWidth,
+        // 定义比例
+        rate = winW / uiW;
+    // 获得最外层盒子
+    var oW = document.getElementById("wrap");
+    if (winW > uiW) {
+        oW.style.width = uiW + "px";
+        oW.style.margin = "0 auto";
+        return;
+    }
+
+    document.documentElement.style.fontSize = rate * 10 + "px"
+}
+// 调用函数
+setRem();
